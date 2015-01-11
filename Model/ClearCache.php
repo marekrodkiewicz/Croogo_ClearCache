df856c53a29a9b9351580bbd0768fcfaa99be9b7
Marek Rodkiewicz <marek.rodkiewicz@infraredgroup.pl>
2015-01-11 20:01:21 +0100

    Cleaning all declared cache independent of engine

--- Model/ClearCache.php
+++ Model/ClearCache.php
@@ -18,34 +18,9 @@ class ClearCache extends ClearCacheAppModel {
 
     public function delete($path = null, $recursive = true) {
 
-        if (!$path) { $path = TMP . 'cache' . DS; }
-
-        $dirItems = scandir($path);
-        $ignore = array('.', '..');
-
-        foreach ($dirItems AS $dirItem) {
-
-            if (in_array($dirItem, $ignore)) continue;
-
-            if (is_dir($path . $dirItem) && $recursive) {
-                $this->delete($path . $dirItem . DS);
-            } elseif (substr($dirItem, 0, 5) == 'cake_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 7) == 'croogo_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 5) == 'type_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 5) == 'types_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 5) == 'node_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 6) == 'nodes_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 11) == 'permission_') {
-                unlink($path . $dirItem);
-            } elseif (substr($dirItem, 0, 12) == 'permissions_') {
-                unlink($path . $dirItem);
-            }
+        $configuredCache = Cache::configured();
+        foreach($configuredCache as $settingName) {
+            Cache::clear(false, $settingName);
         }
     }
 }
